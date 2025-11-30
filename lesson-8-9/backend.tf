87ebad105c9e2e568e38e02ba61485908074ce5d
RomanSemywolos <Roman.Semywolos@gmail.com>
2025-11-30 20:49:34 +0200

    main

--- lesson-8-9/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "lesson-5-2734-9713-5368"
-    key            = "lesson-8-9/terraform.tfstate"
-    region         = "eu-central-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- modules/s3-backend/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "bucket_name" {
-  description = "Name of the S3 bucket for storing Terraform state"
-  type        = string
-}
-
-variable "table_name" {
-  description = "Name of the DynamoDB table for Terraform state locks"
-  type        = string
-}
